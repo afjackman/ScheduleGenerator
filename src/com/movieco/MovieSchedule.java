9a502147fb159113fca2242561a3de344db69e8c
Adam Jackman <4642366+afjackman@users.noreply.github.com>
2017-06-30 12:52:55 -0400

    General object model format created

---
+++ src/com/movieco/MovieSchedule.java
@@ -0,0 +1,13 @@
+package com.movieco;
+
+/**
+ * Represents the schedule created for a particular movie at a particular theater.
+ * Created by adamjackman on 6/30/17.
+ */
+public class MovieSchedule {
+
+    // Prints the movie and the times that the movie will play
+    public String toString() {
+        return "";
+    }
+}
---
+++ src/com/movieco/TheaterSchedule.java
@@ -0,0 +1,19 @@
+package com.movieco;
+
+import java.util.List;
+
+/**
+ * Represents the schedule for a theater on a given day.
+ * Created by adamjackman on 6/30/17.
+ */
+public class TheaterSchedule {
+
+    public TheaterSchedule(List<Movie> movieList, TheaterHours theaterHours, int dayOfWeek) {
+
+    }
+
+    // Prints the theater's schedule
+    public String toString() {
+        return "";
+    }
+}
