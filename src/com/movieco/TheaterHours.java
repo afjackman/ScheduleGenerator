b8515b80a5e1f9464d6438d0f19a57f3703764fc
Adam Jackman <tzveeJ@gmail.com>
2017-06-30 16:09:09 -0400

    Reorganize code and continue developing objects. This is what I showed Phil/Chris.

--- src/com/movieco/TheaterHours.java
+++ src/com/movieco/TheaterHours.java
@@ -6,4 +6,20 @@ package com.movieco;
  */
 public class TheaterHours {
 
+    DailyHours[] dailyHours;
+
+    public TheaterHours() {
+        dailyHours = new DailyHours[7];
+        dailyHours[0] = new DailyHours(10, 30, 23, 30);
+        dailyHours[1] = new DailyHours(8, 0, 23, 0);
+        dailyHours[2] = new DailyHours(8, 0, 23, 0);
+        dailyHours[3] = new DailyHours(8, 0, 23, 0);
+        dailyHours[4] = new DailyHours(8, 0, 23, 0);
+        dailyHours[5] = new DailyHours(10, 30, 23, 30);
+        dailyHours[6] = new DailyHours(10, 30, 23, 30);
+    }
+
+    public DailyHours getDailyHours(int dayOfWeek) {
+        return dailyHours[dayOfWeek];
+    }
 }
--- src/com/movieco/TheaterSchedule.java
+++ src/com/movieco/TheaterSchedule.java
@@ -1,5 +1,6 @@
 package com.movieco;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -8,12 +9,26 @@ import java.util.List;
  */
 public class TheaterSchedule {
 
-    public TheaterSchedule(List<Movie> movieList, TheaterHours theaterHours, int dayOfWeek) {
+    List<MovieSchedule> movieSchedules;
 
+    public TheaterSchedule(List<Movie> movieList, TheaterHours theaterHours, int dayOfWeek) {
+        movieSchedules = new ArrayList<>(movieList.size());
+        DailyHours todaysHours = theaterHours.getDailyHours(dayOfWeek);
+        for (Movie movie : movieList) {
+            movieSchedules.add(new MovieSchedule(movie, todaysHours));
+        }
     }
 
-    // Prints the theater's schedule
+    // Returns the theater's schedule
     public String toString() {
-        return "";
+        StringBuilder schedule = new StringBuilder();
+        for (MovieSchedule movieSchedule : movieSchedules) {
+            Movie movie = movieSchedule.getMovie();
+            schedule.append(movie.toString());
+            schedule.append("\n");
+            schedule.append(movieSchedule.toString());
+            schedule.append("\n\n");
+        }
+        return schedule.toString();
     }
 }
